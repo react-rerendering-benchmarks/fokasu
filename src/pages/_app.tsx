5b412a8aa0e56c72cc6b7f5506964d065a295dda
Satyajit Gokhale <twisterintwist@gmail.com>
2024-10-13 10:33:55 -0400

    Instrumenting React Components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import type { AppProps } from "next/app";
-import "./../global.css"
-
+import "./../global.css";
 
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps
+}: AppProps) {
+  console.log(window.globalCount++);
   return <Component {...pageProps} />;
 }
